613a70622698e4e84df5b0f5285fd1a9b07bb707
Manshi raj <manshi.raj_cs19@gla.ac.in>
2020-08-30 15:42:55 +0530

    create fields for FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -5,5 +5,12 @@
  * */
 package execution;
 
+public class FrontDesk {
+    public static final int EXIT = 4;
+    private static final int ISSUE_A_BOOK = 1;
+    private static final int RETURN_PREVIOUSLY_ISSUED_BOOKS = 2;
+    private static final int SHOW_ALL_BOOKS_ISSUED = 3;
+}
+
 
 
