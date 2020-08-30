bda14dd9acd842368412f114fad52214eb52936a
Manshi raj <manshi.raj_cs19@gla.ac.in>
2020-08-30 12:15:01 +0530

    Create void method for to store the books in @addBooks

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -47,5 +47,16 @@ public class Library {
         return Arrays.hashCode(getBooksCurrentlyAvailable());
     }
 
+    /**
+     * GOING TO CREATE A METHOD, SO THAT ANY ONE IS ABLE TO STORE THE
+     * BOOKS IN THE LIBRARY,
+     *
+     * @addBooks TO STORE THE BOOKS IN
+     */
+
+    public void addBooks(String bookName) {
+        System.out.println(bookName + ",is succesfully added.");
+    }
+
 
 }
