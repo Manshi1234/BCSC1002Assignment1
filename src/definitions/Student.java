7c9f920b63db4c60b30c55444e23085bab96b2e6
Manshi raj <manshi.raj_cs19@gla.ac.in>
2020-08-30 13:00:51 +0530

    add a private fields for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,4 +6,11 @@
 package definitions;
 
 public class Student {
+    private String firstNameOfStudent;
+    private String middleNameOfStudent;
+    private String lastNameOfStudent;
+    private long studentUniversityRollNo;
+    private int numberOfBooksIssued;
+    private Book[] BooksIssuedByStudent;
+
 }
