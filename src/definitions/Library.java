7de77861540e0594fd70bc40cca806ba8ae6f7b5
Manshi raj <manshi.raj_cs19@gla.ac.in>
2020-08-30 11:56:05 +0530

    Create the constructor for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -6,3 +6,28 @@
  * */
 package definitions;
 
+
+public class Library {
+    private Book[] booksCurrentlyAvailable;
+
+    //created the constructor
+    public Library() {
+        this.booksCurrentlyAvailable = new Book[20];
+//        for (int i = 0; i < Book.length(); i++) {
+//
+//        }
+        this.booksCurrentlyAvailable[0] = new Book("Discrete mathematics", "Kneath rosan", "2765931054782");
+        this.booksCurrentlyAvailable[1] = new Book("Oops", "Dusty phillips", "4376589120763");
+        this.booksCurrentlyAvailable[2] = new Book("Python", "Harsh bhasin", "8623498640274");
+    }
+
+    public Book[] getBooksCurrentlyAvailable() {
+        return booksCurrentlyAvailable.clone();
+    }
+
+    public void setBooksCurrentlyAvailable(Book[] booksCurrentlyAvailable) {
+        this.booksCurrentlyAvailable = booksCurrentlyAvailable;
+    }
+
+
+}
