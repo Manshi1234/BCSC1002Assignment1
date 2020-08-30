77c622c742067b55e0a8f61e555f0a836d6d63eb
Manshi raj <manshi.raj_cs19@gla.ac.in>
2020-08-30 12:46:27 +0530

    add a user details

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,7 +1,6 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Manshi Raj
+ *  Date: 30/08/20
  *  File Name : Book.java
  * */
 package definitions;
